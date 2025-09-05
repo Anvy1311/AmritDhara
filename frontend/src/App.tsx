3b512e68f1a2e73880c3c0aa6fcd3185a4eb721e
Anvy1311 <anvymaheshwari@gmail.com>
2025-09-06 01:10:56 +0530

    React + TypeScrip done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Welcome to AyurSutra</h1>
+      <p>Your one-stop solution for Ayurvedic knowledge and resources.</p>
+    </>
+  )
+}
+
+export default App
